fe5638990b9788bd05def9128ac59792e2e0826e
거부길 <sopens@naver.com>
2023-07-13 23:12:45 +0900

    ICT 인턴십 코테 5번 문제
    규칙 찾은 후 공식화

--- Dynamic/antwarrior.py
+++ Dynamic/antwarrior.py
@@ -1,11 +1,13 @@
 n = int(input())
 
-d = [0] * int((n/2))
+array = list(map(int, input().split()))
 
-d[0] = 6
+d = [0] * 100
 
+d[0] = array[0]
+d[1] = max(array[0], array[1])
 
-for i in range(2,int(n/2)):
-    d[i] = d[i-1]*3
+for i in range(2,n):
+    d[i] = max(d[i-1], d[i-2] + array[i])
 
-print(d[len(d)-1])
+print(d[n-1])
---
+++ SolvedSilver/5.py
@@ -0,0 +1,11 @@
+n = 2
+
+d = [0] * int((n/2))
+
+d[0] = 6
+
+
+for i in range(2,int(n/2)):
+    d[i] = d[i-1]*3
+
+print(d[len(d)-1])
